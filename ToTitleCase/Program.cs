b6f2591b55c1c093497ec695c474bc91082288f0
mbm6415 <audrius.budrys@teliacompany.com>
2021-08-22 23:02:38 +0300

    Add project files.

---
+++ ToTitleCase/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConvertToTitleCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tCase = ConvertToTitleCase.TitleCase.ToTitleCase("belekas");
+            Console.WriteLine(tCase);
+        }
+
+    }
+}
---
+++ ToTitleCase/TitleCase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConvertToTitleCase
+{
+    public static class TitleCase
+    {
+        public static string ToTitleCase(this string givenString)
+        {
+            string titleCase = givenString[0].ToString().ToUpper() + givenString[1..];
+            return titleCase;
+        }
+    }
+}
